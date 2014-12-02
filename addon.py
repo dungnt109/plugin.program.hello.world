c755585f7d82f5e0c73f7e6634dff40d910b5108
Zag2me <doowles2@hotmail.com>
2014-12-02 10:18:06 +0000

    [Update] Remove legacy underscores

--- addon.py
+++ addon.py
@@ -1,11 +1,11 @@
 ﻿import xbmcaddon
 import xbmcgui
  
-__addon__       = xbmcaddon.Addon()
-__addonname__   = __addon__.getAddonInfo('name')
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
  
-xbmcgui.Dialog().ok(__addonname__, line1, line2, line3)
+xbmcgui.Dialog().ok(addonname, line1, line2, line3)
